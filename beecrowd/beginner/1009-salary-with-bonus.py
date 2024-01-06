baecf82cf22d6db254fb64429e1eea28f7a1e0ed
mirnatul <www.mirnatul@gmail.com>
2024-01-07 01:04:24 +0600

    1009-salary-with-bonus-solved

---
+++ beecrowd/beginner/1009-salary-with-bonus.py
@@ -0,0 +1,12 @@
+# Md Mirnatul Islam
+# +8801763199000
+# www.mirnatul@gmail.com
+
+name = input("")
+fixed_salary = float(input(""))
+sale = float(input(""))
+
+salary = fixed_salary + sale * (15/100)
+two_decimal_salary = f"{salary:.2f}"
+
+print("TOTAL = R$", two_decimal_salary)
