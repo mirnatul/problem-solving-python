0e044364e9d92f501495fef45972c165899ad042
mirnatul <www.mirnatul@gmail.com>
2024-01-06 22:52:44 +0600

    1005-average-1-solved

--- beecrowd/beginner/1005-average-1.py
+++ beecrowd/beginner/1005-average-1.py
@@ -2,3 +2,10 @@
 # +8801763199000
 # www.mirnatul@gmail.com
 
+a = float(input(""))
+b = float(input(""))
+
+result = ((a*3.5)+(b*7.5))/(3.5+7.5)
+five_decimal = f"{result:.5f}"
+
+print("MEDIA =", five_decimal)
