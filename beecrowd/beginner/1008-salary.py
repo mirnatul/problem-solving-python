7b748b90f5e847399bbfc1d3c6df94d2d590dd95
mirnatul <www.mirnatul@gmail.com>
2024-01-07 00:57:01 +0600

    1007-salary-solved

---
+++ beecrowd/beginner/1008-salary.py
@@ -0,0 +1,13 @@
+# Md Mirnatul Islam
+# +8801763199000
+# www.mirnatul@gmail.com
+
+number = int(input(""))
+work_hour = int(input(""))
+per_hour = float(input(""))
+
+salary = work_hour*per_hour
+two_decimal_salary = f"{salary:.2f}"
+
+print("NUMBER =", number)
+print("SALARY = U$", two_decimal_salary)
