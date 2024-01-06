bae287d41fdf955650e9d643036761001658ee0c
mirnatul <www.mirnatul@gmail.com>
2024-01-07 00:49:00 +0600

    1007-difference-solved

---
+++ beecrowd/beginner/1007-difference.py
@@ -0,0 +1,12 @@
+# Md Mirnatul Islam
+# +8801763199000
+# www.mirnatul@gmail.com
+
+a = int(input(""))
+b = int(input(""))
+c = int(input(""))
+d = int(input(""))
+
+result = (a*b) - (c*d)
+
+print("DIFERENCA =", result)
