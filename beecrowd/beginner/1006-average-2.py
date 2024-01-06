83be75e76c93f25184cd17730447347dd5a27c2a
mirnatul <www.mirnatul@gmail.com>
2024-01-07 00:42:59 +0600

    1006-average-2-solved

---
+++ beecrowd/beginner/1006-average-2.py
@@ -0,0 +1,12 @@
+# Md Mirnatul Islam
+# +8801763199000
+# www.mirnatul@gmail.com
+
+a = float(input(""))
+b = float(input(""))
+c = float(input(""))
+
+result = ((a*2)+(b*3)+(c*5))/(2+3+5)
+one_decimal_result = f"{result:.1f}"
+
+print("MEDIA =", one_decimal_result)
