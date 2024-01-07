cae1e93f01d52ce67ada1eff550f3e07ee57037a
mirnatul <www.mirnatul@gmail.com>
2024-01-07 13:59:41 +0600

    updated-1012

--- beecrowd/beginner/1013-the-greatest.py
+++ beecrowd/beginner/1013-the-greatest.py
@@ -9,9 +9,6 @@ num1 = int(values[0])
 num2 = int(values[1])
 num3 = int(values[2])
 great_12 = int((num1+num2+abs(num1-num2))/2)
-great_23 = int((num2+num3+abs(num2-num3))/2)
+great_all = int((great_12+num3+abs(great_12-num3))/2)
 
-if great_12>great_23:
-    print(great_12, "eh o maior")
-else:
-    print(great_23, "eh o maior")
+print(great_all, "eh o maior")
