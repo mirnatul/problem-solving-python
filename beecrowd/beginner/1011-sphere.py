01d3be39d88736d7b28e29e2806e045aca133b00
mirnatul <www.mirnatul@gmail.com>
2024-01-07 12:42:29 +0600

    1011-sphere-solved

---
+++ beecrowd/beginner/1011-sphere.py
@@ -0,0 +1,11 @@
+# Md Mirnatul Islam
+# +8801763199000
+# www.mirnatul@gmail.com
+
+PI = 3.14159
+r = float(input(""))
+
+volume = (4/3)*PI*pow(r, 3)
+three_decimal_volume = f"{volume:.3f}"
+
+print("VOLUME =", three_decimal_volume)
