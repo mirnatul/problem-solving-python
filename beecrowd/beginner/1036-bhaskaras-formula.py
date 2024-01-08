84b56f79c98adfe2f7b0a8766e37cf720609f60f
mirnatul <www.mirnatul@gmail.com>
2024-01-09 01:04:53 +0600

    1036-complete

---
+++ beecrowd/beginner/1036-bhaskaras-formula.py
@@ -0,0 +1,21 @@
+import math
+a, b, c = list(map(float, input().split()))
+
+delta = round(pow(b, 2)-4*a*c, 2)
+
+if a==0 or delta<0:
+    print("Impossivel calcular")
+else:
+    x1 = f"{(-b+math.sqrt(delta))/(2*a):.5f}"
+    x2 = f"{(-b-math.sqrt(delta))/(2*a):.5f}"
+    print("R1 =", x1)
+    print("R2 =", x2)
+
+
+a=0
+if a==0:
+    print("hello")
+else:
+    b = 7
+    print(b) # b is accessible
+print(b) # b is not accessible
